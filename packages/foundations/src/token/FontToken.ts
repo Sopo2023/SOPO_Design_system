1609c50c40a87e2abcbe47197721916024755e78
해준 <127281057+ftery0@users.noreply.github.com>
2024-07-28 00:24:48 +0900

    fix :: font폴더

--- packages/foundations/src/token/FontToken.ts
+++ packages/foundations/src/token/FontToken.ts
@@ -1,3 +1,50 @@
 import {
-  
-  } from "@sopo-web/assets";
+  PretendardThin,
+  PretendardExtraLight,
+  PretendardLight,
+  PretendardRegular,
+  PretendardMedium,
+  PretendardSemiBold,
+  PretendardBold,
+  PretendardExtraBold,
+  PretendardBlack,
+} from "@sopo-web/assets";
+
+export const SOPOFonts = Object.freeze([
+  {
+    font: "Pretendard-Thin",
+    url: PretendardThin,
+  },
+  {
+    font: "Pretendard-ExtraBold",
+    url: PretendardExtraBold,
+  },
+  {
+    font: "Pretendard-ExtraLight",
+    url: PretendardExtraLight,
+  },
+  {
+    font: "Pretendard-Bold",
+    url: PretendardBold, 
+  },
+  {
+    font: "Pretendard-Light",
+    url: PretendardLight,
+  },
+  {
+    font: "Pretendard-Medium",
+    url: PretendardMedium,
+  },
+  {
+    font: "Pretendard-Regular",
+    url: PretendardRegular,
+  },
+  {
+    font: "Pretendard-SemiBold",
+    url: PretendardSemiBold,
+  },
+  {
+    font: "Pretendard-Black",
+    url: PretendardBlack,
+  },
+]);
