9ebc51ba7331b8a603c541033a5ddb2f0a870ecd
ftery0 <lerb5253@dgsw.hs.kr>
2024-12-12 15:29:38 +0900

    fix :: export

---
+++ index.ts
@@ -0,0 +1,5 @@
+export * from './packages/assets';
+export * from './packages/components';
+export * from './packages/foundations';
+export * from './packages/hooks';
+export * from './packages/styles';
