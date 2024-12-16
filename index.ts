8061aa41f1fc2ab133124219d0764f7682394772
ftery0 <lerb5253@dgsw.hs.kr>
2024-12-16 22:53:12 +0900

    chore :: package error을 위한..

--- index.ts
+++ index.ts
@@ -1,5 +1,1 @@
-export * from './packages/assets';
-export * from './packages/components';
-export * from './packages/foundations';
-export * from './packages/hooks';
-export * from './packages/styles';
+export * from './packages';
